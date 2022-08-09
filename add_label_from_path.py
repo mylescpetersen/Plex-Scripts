c9b5da4fee97f233ef90c02dd688177cbe0b35cf
Myles Petersen <53098879+mylescpetersen@users.noreply.github.com>
2022-08-09 03:40:03 -0700

    Added if name == main

--- add_label_from_path.py
+++ add_label_from_path.py
@@ -6,53 +6,59 @@ import re
 # Plex script to find all shows & movies whose path matches a specified regex, and then adds a specified sharing label to each
 # ----------------------------------------------------------------------------------------------------------------------------
 
-# --- Edit these ---
-plexURL = 'http://<local-plex-ip>:32400'
-plexToken = '<plex-token>'
-# A list of library names (strings) to search
-libraries = ["Movies", "TV Shows"]
-# The sharing label to add to each Show/Movie 
-label_to_add = "<label to add to each show/movie>"
-# Regex that will search against each Show/Movie's path
-path_search_regex = "<regex to search filepaths for>"
+def main():
 
-plex = PlexServer(plexURL, plexToken)
+    # --- Edit these ---
+    plexURL = 'http://<local-plex-ip>:32400'
+    plexToken = '<plex-token>'
+    # A list of library names (strings) to search
+    libraries = ["Movies", "TV Shows"]
+    # The sharing label to add to each Show/Movie 
+    label_to_add = "<label to add to each show/movie>"
+    # Regex that will search against each Show/Movie's path
+    path_search_regex = "<regex to search filepaths for>"
 
-# Updated by getMediaFromPath, List of Videos (Shows/Movies) to add the sharing tag to
-videos = []
+    plex = PlexServer(plexURL, plexToken)
 
-# Updates videos list
-def getMediaFromPath(library, videos=videos):
+    # Updated by getMediaFromPath, List of Videos (Shows/Movies) to add the sharing tag to
+    videos = []
 
-    for video in plex.library.section(library).search():
-      
-        # Succeeds if a Movie library
-        try:
-            # Gets path of the movie file
-            file_path = video.media[0].parts[0].file
+    # Updates videos list
+    def getMediaFromPath(library, videos=videos):
 
-        # Throws if a TV Show library   
-        except AttributeError:
-            
-            # Gets path of TV Show folder
-            file_path = video.locations[0]
+        for video in plex.library.section(library).search():
 
-        # Regex search the path of each show/movie
-        if re.search(path_search_regex, file_path):
-            videos.append(video)
+            # Succeeds if a Movie library
+            try:
+                # Gets path of the movie file
+                file_path = video.media[0].parts[0].file
 
-# Searches all libraries in the libraries list for shows/movies matching the desired path search'
-for library in libraries:
-    getMediaFromPath(library)
+            # Throws if a TV Show library   
+            except AttributeError:
 
-# Add the sharing label to each item
-for v in videos:
-    v.addLabel(label_to_add)
+                # Gets path of TV Show folder
+                file_path = video.locations[0]
 
-# Print all affected movies, shows
-for v in videos:
-    print("%s - %s" % (v.title, v.type))
+            # Regex search the path of each show/movie
+            if re.search(path_search_regex, file_path):
+                videos.append(video)
 
+    # Searches all libraries in the libraries list for shows/movies matching the desired path search'
+    for library in libraries:
+        getMediaFromPath(library)
+
+    # Add the sharing label to each item
+    for v in videos:
+        v.addLabel(label_to_add)
+
+    # Print all affected movies, shows
+    for v in videos:
+        print("%s - %s" % (v.title, v.type))
+
+    
+    
+if __name__ == "__main__":
+    main()
 
 
 
